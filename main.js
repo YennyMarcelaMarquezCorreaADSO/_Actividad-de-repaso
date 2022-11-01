472b25c78cbf1a51304701c2c8e571435fe38c36
YennyMarcelaMarquezCorreaADSO <yennycorrea0911@gmail.com>
2022-11-01 10:04:31 -0500

    Ejercicio 2 - corrección

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
 addEventListener("DOMContentLoaded", (e)=>{
-    let pag = dovument.querySelector('#pago')
+    let pag = document.querySelector('#pago')
     let hora = document.querySelector("#myForm");
     let ventane = document.querySelector("dialog");
     let Close = document.querySelector("#close");
     let cont = 0
+
 
     
     hora.addEventListener("submit", (e)=>{
         e.preventDefault();
         let dataInput = Object.fromEntries(new FormData(e.target));
         let preciohora = 5208.33
-        let myhora = dataInput.hora
-        myhora*=preciohora
-        D = Math.round(myhora)
-        pag.innerHTML = myhora
+        let mihora = dataInput.hora
+        mihora*=preciohora
+        tot = Math.round(mihora)
+        pag.innerHTML = mihora
+
+        ventane.showModal();
+        if(cont < 50) {
+            let mytabla = document.querySelector('tbody');
+            mytabla.insertAdjacentHTML('beforeend',`
+            <tr>
+                <td>${dataInput.nombre}</td>
+                <td>${dataInput.hora}</td>
+                <td>${tot}</td>
+                <td>${tot*30}</td>
+            </tr>
+            `)
+            cont++;
+        }
     })
 
     Close.addEventListener("click", (e)=>{
         ventane.close();
     })
+
+
 
 })
