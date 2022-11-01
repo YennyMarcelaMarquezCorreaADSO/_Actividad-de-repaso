eb31d41e9c1d697dcbed35a003ab890de1c8eb7f
YennyMarcelaMarquezCorreaADSO <yennycorrea0911@gmail.com>
2022-11-01 12:23:34 -0500

    Ejercicio 4

--- main.js
+++ main.js
@@ -1,39 +1,42 @@
 addEventListener("DOMContentLoaded", (e)=>{
 
-let n = document.querySelector("#N")
-let resultado = ""
-let numeros = document.querySelector("#Numeros")
-let suma = 0
-let res = document.querySelector("#resultados")
+let ventas  = document.querySelector("#ventas")
+let res = document.querySelector("#resultado")
+let vent1 = document.querySelector("#venta1")
+let vent2 = document.querySelector("#venta2")
+let vent3 = document.querySelector("#venta3")
+let ventane = document.querySelector("dialog")
+let Close = document.querySelector("#close")
+let sueldoBase = 475800
 
 
-numeros.addEventListener("submit", (e)=>{
+ventas.addEventListener("submit", (e)=>{
     e.preventDefault();
+    let dataInput = Object.fromEntries(new FormData(e.target))
+    let comision1 = Number(dataInput.vent1)*0.10
+    let comision2 = Number(dataInput.vent2)*0.10
+    let comision3 = Number(dataInput.vent3)*0.10
+    let comision = comision1 + comision2 + comision3
+    let totalcomision = sueldoBase + comision
+    res.innerHTML = totalcomision
 
-    let datosEntrada = Object.fromEntries(new FormData(e.target));
+        ventane.showModal();
+        let mytabla = document.querySelector('tbody');
+        mytabla.innerHTML = ""
+        mytabla.insertAdjacentHTML('beforeend',`
+        <tr>
+            <td>${dataInput.venta1}</td>
+            <td>${dataInput.venta2}</td>
+            <td>${dataInput.venta3}</td>
+            <td>${sueldoBase}</td>
+            <td>${totalcomision}</td>
+        </tr>
+        `)
 
-    for (let s = 1; s < datosEntrada.N; s++){
+    })
 
-        if(datosEntrada.N % s == 0){
-            suma+=s
-        }
-    }
-
-    if(datosEntrada.N == suma){
-        resultado = "Es perfecto"
-        suma = 0
-    }
-
-    else{
-        resultado = "No es perfecto"
-        suma = 0
-    }
-
-    res.innerHTML = ""
-    res.insertAdjacentHTML("beforeend",`
-        <h2> ${datosEntrada.N + ' ' + resultado} </h2>
-    `);
-
+    Close.addEventListener("click", (e)=>{
+        ventane.close();
     })
 
 
